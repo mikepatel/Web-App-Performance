3ec65a490983c9d4745a783d015465990870b4a2
Michael <michael.patel94@gmail.com>
2022-11-24 00:48:18 -0500

    Adding about 5MB response payload

--- flask_web_app/run.py
+++ flask_web_app/run.py
@@ -8,15 +8,23 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/get", methods=["GET"])
+@app.route("/data", methods=["GET"])
 def get_data():
-    data = {
-
-    }
+    data = _helper()
     return data
+
+
+################################################################################
+# Helper function
+def _helper():
+    output = []
+    for i in range(100000):
+        output.append({"Index": i, "Value": i*i})
+
+    return output
 
 
 ################################################################################
 # Main
 if __name__ == "__main__":
-    app.run(host="localhost", debug=True)
+    app.run(host="localhost", port=8000, debug=True)
